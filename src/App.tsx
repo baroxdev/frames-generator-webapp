c9f54a0fb9504f3cb752d8a530d373af41a0d4b7
baroxdev <barox.dev@gmail.com>
2024-07-07 09:42:46 +0700

    feat: Add lazy loading for images in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -316,9 +316,9 @@ function App() {
         background: `url(${backgroundHorizontial})`,
       }}
     >
-      {true && (
+      {showMockImage && (
         <div className="overflow-hidden max-md:hidden">
-          <div className="absolute top-0 left-0 z-[999]" ref={cardRef}>
+          <div className="absolute top-0 left-0 z-[-1]" ref={cardRef}>
             <img src={backgroundImage} width={1500} height={843} />
             <div>
               <div className="absolute bottom-[175px]  left-[100px]">
